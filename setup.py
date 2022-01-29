b1663919b57605a8dec82c8834d5f4bb6cef270b
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2022-01-29 12:46:04 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name='aio',
+    version='0.1.0',
+    description='The purpose of this package is to provide asynchronous variants of the builtin `input` and `print` functions.',
+    packages=['aio'],
+    python_requires='>=3.7',
+    url="https://github.com/SimpleArt/aio",
+    author="Jack Nguyen",
+    author_email="jackyeenguyen@gmail.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    classifier=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    license_files=["LICENSE"],
+)

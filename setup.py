8b76d06d5b6cc3a093d6546b318035e454cd1cc1
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2022-01-29 13:36:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
